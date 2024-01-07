d8a896dec47594fce6ebf84d297a0eb91ae45280
chelsealaurenrose <chelsealaurenrose@gmail.com>
2024-01-08 11:05:59 +1300

    Created the GameState enumerator.

---
+++ Original Assignment 2 code/src/GameState.java
@@ -0,0 +1,12 @@
+/* this enumerator represents the differents states the game can be in. 
+ * playing = game is ongoing; draw = game is a draw; cross_won = cross won the game; 
+ * nought_won = nought won the game. 
+ */
+public enum GameState {
+	
+	Playing, 
+	Draw, 
+	Cross_won,
+	Nought_won
+
+}
