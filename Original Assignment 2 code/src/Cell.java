0383297c6fce9ebb335300fd5eeb2af5fb7d4787
chelsealaurenrose <chelsealaurenrose@gmail.com>
2024-01-08 10:53:49 +1300

    New branch created for the cell class. Initialised the variables row and column. Called the clear() method. Set content to player.empty.

--- Original Assignment 2 code/src/Cell.java
+++ Original Assignment 2 code/src/Cell.java
@@ -4,32 +4,41 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 public class Cell {
-    //content of this cell (empty, cross, nought)
+    /*Declares the variable 'content' of type Player which 
+	denotes the content of the cell (empty, cross, nought)*/
 	Player content;
-	//row and column of this cell
+	//Variables store the row and column of this cell within the game grid.
 	int row, col;
 	
-	/** Constructor to initialise this cell with the specified row and col */
+	/* Constructor to initialise the cell object with the specified row and col - 
+	 * sets the row and column variables to provided values. Calls the clear() method
+	 * to set cell's content to empty */
 	public Cell(int row, int col) {
 		
-		// TODO: Initialise the variables row, col 
+		// DONE: initialise the variables row, col 
+		this.row = row; 
+		this.col = col; 
 
-		
-		
-		//TODO: call the method that sets the cell content to EMPTY
-		 
+		//DONE: call the method that sets the cell content to EMPTY
+		clear(); 		 
 	}
 	
 
-	/** Paint itself on the graphics canvas, given the Graphics context g */ 
+	/* Paint itself on the graphics canvas, given the Graphics context g
+	 * The paint(Graphics g) method draws the cell on the graphics canvas*/
+	
 	public void paint(Graphics g) {
 		//Graphics2D allows setting of pen's stroke size
 		Graphics2D graphic2D = (Graphics2D) g;
 		graphic2D.setStroke(new BasicStroke(GameMain.SYMBOL_STROKE_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
 		
-		//draw the symbol in the position
+		//Coordinates are determined to draw nought or cross based on cell's position. 
+	
 		int x1 = col * GameMain.CELL_SIZE + GameMain.CELL_PADDING;
 		int y1 = row * GameMain.CELL_SIZE + GameMain.CELL_PADDING;
+		/* if content ==Player.Cross it sets colour to red and draws the cross. 
+		 * if content == Player.Nought it sets colour to blue and draws the circle. 
+		 */
 		if (content == Player.Cross) {
 			graphic2D.setColor(Color.RED);
 			int x2 = (col + 1) * GameMain.CELL_SIZE - GameMain.CELL_PADDING;
@@ -45,9 +54,8 @@ public class Cell {
 	/** Set this cell's content to EMPTY */
 	public void clear() {
 		
-		// TODO: Set the value of content to Empty (Remember this is an enum)
-
-		
+		// DONE: Set the value of content to Empty (Remember this is an enum)
+		content = Player.Empty; 		
 	}
 		
 }
